ec5bf8bccc431c05f22c70d3e9aa028e36b42362
bhargav-garlapati <bhargav.garlapati@telus.com>
2020-12-03 14:39:53 -0800

    goole fail search with Telus keyword

--- cypress/integration/google-fail.js
+++ cypress/integration/google-fail.js
@@ -6,12 +6,12 @@ describe('Google fail search', () => {
 
     it('Search for something strange', () => {
         cy.get('[name="q"]')
-        .type('anqqaxc a57k2..')
-        .should('have.value', 'anqqaxc a57k2..')
+        .type('telus')
+        .should('have.value', 'telus')
         .type('{enter}')
     })
 
     it('Expect more than 1000 results', () => {
-        cy.get('[id="result-stats"]').contains('100 results')
+        cy.get('[id="result-stats"]').contains('1000 results')
     })
   })
