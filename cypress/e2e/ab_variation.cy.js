37658adb3e8251fcfdc1fa0491866ed677a34753
Bhargav Garlapati <16038883+bhargav-garlapati@users.noreply.github.com>
2024-11-22 23:31:26 -0800

    feat(abvariation): ab test added

---
+++ cypress/e2e/ab_variation.cy.js
@@ -0,0 +1,18 @@
+describe('Welcome to the-internet', () => {
+    const data = require('../fixtures/content.json');
+
+    it('A/B Test Variation', () => {
+
+        // Visit login page
+        cy.visitPage('/abtest');
+
+        // Check h3 text
+        cy.get('body').then(($body) => {
+            if ($body.find('h3').text().includes(data.abtest1)) {
+                cy.log(data.abtest1);
+            } else {
+                cy.log(data.abtest2);
+            }
+        })
+    })
+})
