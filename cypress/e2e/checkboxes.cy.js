8a6f32126bc411433a44ed4f3b299935c02de266
Bhargav Garlapati <16038883+bhargav-garlapati@users.noreply.github.com>
2024-11-22 23:33:03 -0800

    feat(checkboxes): check if checkbox is checked or not

---
+++ cypress/e2e/checkboxes.cy.js
@@ -0,0 +1,19 @@
+describe('Welcome to the-internet', () => {
+    const data = require('../fixtures/content.json');
+
+    it('Checkboxes', () => {
+
+        // Visit login page
+        cy.visitPage('/checkboxes');
+
+        // Check h3 text
+        cy.get('h3')
+        .contains(data.checkboxes);
+
+        // Check checkbox1 is not checked
+        cy.get('[id="checkboxes"] [type="checkbox"]').eq(0).should('not.be.checked');
+
+        // Check checkbox2 is checked
+        cy.get('[id="checkboxes"] [type="checkbox"]').eq(1).should('be.checked');
+    })
+})
