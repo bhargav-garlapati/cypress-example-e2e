941af7ff2234cdab56c1fc2d5cf453da64f02d99
Bhargav Garlapati <16038883+bhargav-garlapati@users.noreply.github.com>
2024-11-22 23:32:08 -0800

    feat(adddelete): adding and removing elements

---
+++ cypress/e2e/add_delete_elements.cy.js
@@ -0,0 +1,24 @@
+describe('Welcome to the-internet', () => {
+    const data = require('../fixtures/content.json');
+
+    it('Add and Remove elements on the page', () => {
+
+        let times = 7;
+        // Visit login page
+        cy.visitPage('/add_remove_elements/');
+
+        cy.get('h3')
+        .contains(data.addRemoveElements);
+
+        // Click Add Element button 7 times 
+       for (let i = 0; i < times; i++) {
+            cy.get('[onclick="addElement()"]').click();
+        }
+
+        // Click Delete Element button 7 times
+        for (let i = 0; i < times; i++) {
+            cy.get('[onclick="deleteElement()"]').first().click();
+        }
+
+    })
+})
